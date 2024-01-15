c914e9da038c13d237e80226a262b75e0e07bd64
Maxi Pelizzoni <maxi96p@gmail.com>
2024-01-15 15:32:35 -0300

    Button component 🛎️

---
+++ app/src/main/java/com/maxidev/weather/ui/components/WeatherButton.kt
@@ -0,0 +1,39 @@
+package com.maxidev.weather.ui.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WeatherButton(
+    modifier: Modifier = Modifier,
+    @StringRes text: Int,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(4.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Button(
+            onClick = onClick,
+            elevation = ButtonDefaults.elevatedButtonElevation(8.dp)
+        ) {
+            Text(
+                text = stringResource(id = text)
+            )
+        }
+    }
+}
