90fa1480acb38c6dc76705dbb688bfce06acd988
Maxi Pelizzoni <maxi96p@gmail.com>
2024-01-15 15:32:05 -0300

    Modified to place Lottie animations 🖼️

--- app/src/main/java/com/maxidev/weather/ui/presentation/components/ErrorScreen.kt
+++ app/src/main/java/com/maxidev/weather/ui/presentation/components/ErrorScreen.kt
@@ -1,42 +1,59 @@
 package com.maxidev.weather.ui.presentation.components
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.WifiOff
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.maxidev.weather.R
+import com.maxidev.weather.ui.components.WeatherButton
 
 // Simulates an error screen when you do not have internet access.
 @Composable
-fun ErrorScreen(@StringRes errorText: Int) {
+fun ErrorScreen(
+    @StringRes errorText: Int,
+    onClick: () -> Unit
+) {
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.fail_weather)
+    )
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Icon(
-            imageVector = Icons.Outlined.WifiOff,
-            contentDescription = null,
-            modifier = Modifier
-                .size(70.dp)
+        Spacer(modifier = Modifier.weight(1f))
+        LottieAnimation(
+            composition = composition,
+            progress = { progress }
         )
         Text(
             text = stringResource(id = errorText),
-            //textAlign = TextAlign.Justify,
             style = MaterialTheme.typography.titleMedium
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        WeatherButton(
+            text = R.string.re_connect,
+            onClick = onClick
         )
     }
 }
--- app/src/main/java/com/maxidev/weather/ui/presentation/components/LoadingScreen.kt
+++ app/src/main/java/com/maxidev/weather/ui/presentation/components/LoadingScreen.kt
@@ -4,22 +4,42 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.maxidev.weather.R
 
 // Simulate a loading screen.
 @Composable
 fun LoadingScreen() {
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.loading_weather)
+    )
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        content = {
-            CircularProgressIndicator()
-        }
-    )
+        verticalArrangement = Arrangement.Center
+    ) {
+                LottieAnimation(
+            composition = composition,
+            progress = { progress },
+            modifier = Modifier
+                .size(250.dp)
+        )
+    }
 }
