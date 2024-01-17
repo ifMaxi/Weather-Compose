de74f559a24e9e106d73b9920863fe3683579e38
Maxi Pelizzoni <maxi96p@gmail.com>
2024-01-17 15:44:32 -0300

    Fake data

---
+++ app/src/main/java/com/maxidev/weather/utils/ClimatePhrases.kt
@@ -0,0 +1,16 @@
+package com.maxidev.weather.utils
+
+object ClimatePhrases {
+    val listOfPhrases = listOf(
+        "“There is no such thing as good weather or bad weather. There is only weather and your attitude towards it.” Louise Hay.",
+        "“There is no such thing as bad weather, only soft people.” Bill Bowerman.",
+        "“You become the climate you live in.” Iain Pears.",
+        "“Not once has life or the climate complained about a human being.” Mokokoma Mokhonoana.",
+        "“The climate conversation is the last refuge of the unimaginative.” Oscar Wilde.",
+        "“Climate change is the speech of fools.” Thomas Fuller.",
+        "“It is impossible, at least for me, to be poetic in a cold climate.” George Eliot.",
+        "“Climate is a literary specialty, and no inexperienced hand can produce a good article on it.” Mark Twain.",
+        "“I like the idea of giving thanks in the name of the climate.” Yasunari Kawabata.",
+        "“The world is not based on logic, it is based on the seven deadly sins and the weather.” Alan Furst."
+    ).random()
+}
