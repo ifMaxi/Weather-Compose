32d6835d86bbbba1beac77f1b91c953a1e57c221
Maxi Pelizzoni <maxi96p@gmail.com>
2024-01-18 14:44:42 -0300

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    // Core
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 
     // Compose
