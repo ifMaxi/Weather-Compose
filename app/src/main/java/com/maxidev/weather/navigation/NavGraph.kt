8964bd1c04156a28069c5a0698bd45320f7253a6
Maxi Pelizzoni <maxi96p@gmail.com>
2024-01-22 15:19:46 -0300

    Animation transition

--- app/src/main/java/com/maxidev/weather/navigation/NavGraph.kt
+++ app/src/main/java/com/maxidev/weather/navigation/NavGraph.kt
@@ -1,5 +1,12 @@
 package com.maxidev.weather.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -29,7 +36,25 @@ fun NavGraph(
                 }
             )
         }
-        composable(route = secondDestination) {
+        composable(
+            route = secondDestination,
+            enterTransition = {
+                fadeIn(
+                    animationSpec = tween(300, easing = EaseIn)
+                ) + slideIntoContainer(
+                    animationSpec = tween(300, easing = EaseIn),
+                    towards = AnimatedContentTransitionScope.SlideDirection.Start
+                )
+            },
+            exitTransition = {
+                fadeOut(
+                    animationSpec = tween(300, easing = LinearEasing)
+                ) + slideOutOfContainer(
+                    animationSpec = tween(300, easing = EaseOut),
+                    towards = AnimatedContentTransitionScope.SlideDirection.End
+                )
+            }
+        ) {
             WeatherScreen()
         }
     }
