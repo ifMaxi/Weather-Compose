9264cebef9185aa283de8f29c48b8361376f6876
Maxi Pelizzoni <maxi96p@gmail.com>
2024-01-22 21:50:38 -0300

    Removed downloaded fonts, added normal fonts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended:1.5.4")
-    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.4")
+    //implementation("androidx.compose.ui:ui-text-google-fonts:1.5.4")
 
     // Viewmodel
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
--- app/src/main/java/com/maxidev/weather/ui/theme/Type.kt
+++ app/src/main/java/com/maxidev/weather/ui/theme/Type.kt
@@ -2,28 +2,16 @@ package com.maxidev.weather.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.googlefonts.Font
-import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
 import com.maxidev.weather.R
 
-// Downloaded fonts
-val provider = GoogleFont.Provider(
-    providerAuthority = "com.google.android.gms.fonts",
-    providerPackage = "com.google.android.gms",
-    certificates = R.array.com_google_android_gms_fonts_certs
-)
-
-val montserratFont = GoogleFont("Montserrat")
-
-val montserratFamily = FontFamily(
-    Font(googleFont = montserratFont, fontProvider = provider, weight = FontWeight.Light),
-    Font(googleFont = montserratFont, fontProvider = provider, weight = FontWeight.Normal),
-    Font(googleFont = montserratFont, fontProvider = provider, weight = FontWeight.Medium),
-    Font(googleFont = montserratFont, fontProvider = provider, weight = FontWeight.SemiBold),
-    Font(googleFont = montserratFont, fontProvider = provider, weight = FontWeight.Bold)
+val soraFamily = FontFamily(
+    Font(R.font.sora, FontWeight.Normal),
+    Font(R.font.sora_medium, FontWeight.Medium),
+    Font(R.font.sora_semibold, FontWeight.SemiBold)
 )
 
 // Set of Material typography styles to start with
