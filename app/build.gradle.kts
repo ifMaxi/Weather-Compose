0cb6f77d6138da8f10c22309ff6db7b730e99505
Maxi Pelizzoni <maxi96p@gmail.com>
2024-01-24 15:28:47 -0300

    Changes and update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,13 +107,15 @@ dependencies {
 
     // Test
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
-    // Mockito
-    testImplementation("org.mockito:mockito-core:5.9.0")
+    // MockK
+    testImplementation("io.mockk:mockk:1.13.9")
+    testImplementation("io.mockk:mockk-android:1.13.9")
 
     // Debug
     debugImplementation("androidx.compose.ui:ui-tooling")
